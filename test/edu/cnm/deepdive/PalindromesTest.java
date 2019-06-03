6d233c6622d1b1a9bb52b2360f3ff5098a91a272
Ahmad Musleh <amusleh1@cnm.edu>
2019-06-03 10:55:49 -0600

    Initial commit

---
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PalindromesTest {
+
+  @Test
+  public void isPalindroe() {
+    assertTrue(Palindromes.isPalindromes(s: "A man, a plan, a canal"));
+
+  }
+}
